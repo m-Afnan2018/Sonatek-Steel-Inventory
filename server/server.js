5f452d6978c21a1c9ffac5b2e396f18a4c479f65
m-afnan2018 <m.afnan2018@gmail.com>
2025-10-10 17:46:02 +0530

    Updated

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ app.use(express.json());
 app.use(bodyParser.json())
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
-    // origin: process.env.CORS_URL,
+    // origin: 'http://localhost:3000',
+    origin: `${process.env.CORS_URL}`,
     credentials: true,
 }))
 
